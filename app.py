c66c948211925439d5aa0982fb4da86bce0ac1ed
Demiurgo <alanpalacio64@hotmail.com>
2020-07-11 16:12:40 -0500

    map

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import plotly.express as px
 import pandas as pd
 import json
 
-df = pd.read_csv('data/data-final.csv')
+df = pd.read_csv('data/data-final-depurado-fecha.csv')
+df.fillna(value={'FAMI_ESTRATOVIVIENDA':'-'}, inplace=True)
+array=[str(x)[:4] for x in df['PERIODO-X'].values]
+df['YEAR']=[int(x) for x in array]
 
 avalaible_test=df.PRUEBA.unique()
 
@@ -47,13 +50,15 @@ app.layout = html.Div([
     html.Div([
     dcc.Slider(
         id='year-slider',
-        min=df['PERIODO'].min(),
-        max=df['PERIODO'].max(),
-        value=df['PERIODO'].min(),
-        marks={str(year): str(year) for year in df['PERIODO'].unique()},
+        min=df['YEAR'].min(),
+        max=df['YEAR'].max(),
+        value=df['YEAR'].min(),
+        marks={str(year): str(year) for year in df['YEAR'].unique()},
         step=None
     )]),
+    html.Div([
     dcc.Graph(id='map')
+    ])
 ])
 
 
@@ -63,9 +68,9 @@ app.layout = html.Div([
     Input('year-slider', 'value'),
     Input('yaxis-column', 'value')])
 def update_figure(selected_test,selected_year, selected_mod):
-    filtered_df = df[(df.PERIODO == selected_year) & (df.PRUEBA == selected_test)]
-
-    fig = px.box(filtered_df, x="FAMI_ESTRATOVIVIENDA", y=selected_mod, color="FAMI_ESTRATOVIVIENDA")
+    filtered_df = df[(df['YEAR'] == selected_year) & (df.PRUEBA == selected_test)]
+    estratos={'FAMI_ESTRATOVIVIENDA':['Estrato 0', 'Estrato 1', 'Estrato 2', 'Estrato 3', 'Estrato 4', 'Estrato 5','Estrato 6', 'Sin Estrato', 'Zona Rural', '-']}
+    fig = px.box(filtered_df, x="FAMI_ESTRATOVIVIENDA", y=selected_mod, color="FAMI_ESTRATOVIVIENDA", category_orders=estratos)
 
     fig.update_layout(transition_duration=2)
 
@@ -80,12 +85,11 @@ def update_figure(selected_test,selected_year, selected_mod):
     Input('year-slider', 'value'),
     Input('yaxis-column', 'value')])
 def update_map(selected_test,selected_year, selected_mod):
-    #df1=pd.DataFrame({'id':["ANTIOQUIA", "ATLANTICO"],'valor':[3, 2]})
-    filtered_df = df[(df.PERIODO == selected_year) & (df.PRUEBA == selected_test)]
-    df1=filtered_df.groupby('ESTU_PRGM_DEPARTAMENTO').count().reset_index()
-    
+    filtered_df = df[(df['YEAR'] == selected_year) & (df.PRUEBA == selected_test)]
+    df1=filtered_df.groupby('ESTU_PRGM_DEPARTAMENTO').mean().reset_index()
+    limite=df1[selected_mod].max()
     fig = px.choropleth_mapbox(df1, geojson=deparments, locations='ESTU_PRGM_DEPARTAMENTO', color=selected_mod, color_continuous_scale="Viridis",
-    center={'lat':4.6683288,'lon':-74.1350578}, mapbox_style="carto-positron", zoom=4)
+    center={'lat':4.6683288,'lon':-74.1350578}, mapbox_style="carto-positron", zoom=4, range_color=[0, limite])
     fig.update_layout(title="Colombia",margin={"r":0,"t":0,"l":0,"b":0}, paper_bgcolor="#F8F9F9", plot_bgcolor="#F8F9F9")
     return fig
 
