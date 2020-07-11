f01e4bbafdbfee33b4e4af47efcb288c3a22c754
Demiurgo <alanpalacio64@hotmail.com>
2020-07-11 00:17:23 -0500

    connected map

--- app.py
+++ app.py
@@ -76,11 +76,16 @@ def update_figure(selected_test,selected_year, selected_mod):
 
 @app.callback(
     Output('map', 'figure'),
-    [Input('year-slider','value')])
-def update_map(selected_year):
-    df1=pd.DataFrame({'id':["ANTIOQUIA", "ATLANTICO"],'valor':[3, 2]})
-    fig = px.choropleth_mapbox(df1, geojson=deparments, locations='id', color='valor', color_continuous_scale="Viridis",
-    range_color=(0, 12), center={'lat':4.6683288,'lon':-74.1350578}, mapbox_style="carto-positron", zoom=4)
+    [Input('filter-test', 'value'),
+    Input('year-slider', 'value'),
+    Input('yaxis-column', 'value')])
+def update_map(selected_test,selected_year, selected_mod):
+    #df1=pd.DataFrame({'id':["ANTIOQUIA", "ATLANTICO"],'valor':[3, 2]})
+    filtered_df = df[(df.PERIODO == selected_year) & (df.PRUEBA == selected_test)]
+    df1=filtered_df.groupby('ESTU_PRGM_DEPARTAMENTO').count().reset_index()
+    
+    fig = px.choropleth_mapbox(df1, geojson=deparments, locations='ESTU_PRGM_DEPARTAMENTO', color=selected_mod, color_continuous_scale="Viridis",
+    center={'lat':4.6683288,'lon':-74.1350578}, mapbox_style="carto-positron", zoom=4)
     fig.update_layout(title="Colombia",margin={"r":0,"t":0,"l":0,"b":0}, paper_bgcolor="#F8F9F9", plot_bgcolor="#F8F9F9")
     return fig
 
