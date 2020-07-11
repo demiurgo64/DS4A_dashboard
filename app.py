fd65a9ae3a7499ac1c8bc559b30524a9da3588ac
Demiurgo <alanpalacio64@hotmail.com>
2020-07-10 23:59:06 -0500

    first map

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 import pandas as pd
+import json
 
 df = pd.read_csv('data/data-final.csv')
 
 avalaible_test=df.PRUEBA.unique()
 
 avalaible_module=["MOD_COMUNI_ESCRITA_PUNT","MOD_COMPETEN_CIUDADA_PUNT","MOD_LECTURA_CRITICA_PUNT","MOD_RAZONA_CUANTITAT_PUNT","MOD_INGLES_PUNT"]
+
+with open('data/colombiaID.geo.json') as json_file:
+    deparments = json.load(json_file)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -40,6 +44,7 @@ app.layout = html.Div([
     ]),    
 
     dcc.Graph(id='graph-with-slider'),
+    html.Div([
     dcc.Slider(
         id='year-slider',
         min=df['PERIODO'].min(),
@@ -47,7 +52,8 @@ app.layout = html.Div([
         value=df['PERIODO'].min(),
         marks={str(year): str(year) for year in df['PERIODO'].unique()},
         step=None
-    )
+    )]),
+    dcc.Graph(id='map')
 ])
 
 
@@ -65,6 +71,17 @@ def update_figure(selected_test,selected_year, selected_mod):
 
     fig.update_yaxes(title=selected_mod) 
 
+    return fig
+
+
+@app.callback(
+    Output('map', 'figure'),
+    [Input('year-slider','value')])
+def update_map(selected_year):
+    df1=pd.DataFrame({'id':["ANTIOQUIA", "ATLANTICO"],'valor':[3, 2]})
+    fig = px.choropleth_mapbox(df1, geojson=deparments, locations='id', color='valor', color_continuous_scale="Viridis",
+    range_color=(0, 12), center={'lat':4.6683288,'lon':-74.1350578}, mapbox_style="carto-positron", zoom=4)
+    fig.update_layout(title="Colombia",margin={"r":0,"t":0,"l":0,"b":0}, paper_bgcolor="#F8F9F9", plot_bgcolor="#F8F9F9")
     return fig
 
 if __name__ == '__main__':
