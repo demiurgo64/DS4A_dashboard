127feee6e24930a7b3c8dd53aafa0ee415320741
Juan Gonzalez <jusgomen@gmail.com>
2020-07-17 16:29:15 -0500

    reorganize components

--- app.py
+++ app.py
@@ -75,91 +75,261 @@ available_estrato = [
     "Zona Rural"
 ]
 
+# Factores
+available_factor = [
+    'estu_nucleo_pregrado',
+    'estu_prgm_departamento',
+    'estu_genero',
+    'fami_estratovivienda',
+    'inst_caracter_academico',
+    'estu_metodo_prgm',
+    'inst_origen',
+    'estu_valormatriculauniversidad',
+    'estu_simulacrotipoicfes',
+    'estu_horassemanatrabaja'
+]
+
+# Género
+available_gender = [
+    'F',
+    'M'
+]
+
+# Horas Semana Trabaja
+available_work_week_hours = [
+    0,
+    "Menos de 10 horas",
+    "Entre 11 y 20 horas",
+    "Entre 21 y 30 horas",
+    "Más de 30 horas"
+]
+
+# Si No
+available_yes_no = [
+    'Si',
+    'No'
+]
+
+# Valor Matrícula Universidad
+available_tuition_cost = [
+    'No pagó matrícula',
+    'Menos de 500 mil',
+    'Entre 500 mil y menos de 1 millón',
+    'Entre 1 millón y menos de 2.5 millones',
+    'Entre 2.5 millones y menos de 4 millones',
+    'Entre 4 millones y menos de 5.5 millones',
+    'Entre 5.5 millones y menos de 7 millones',
+    'Más de 7 millones'
+]
+
+# Metodo
+available_method = [
+    'PRESENCIAL',
+    'SEMI-PRESENCIAL',
+    'DISTANCIA',
+    'DISTANCIA VIRTUAL' 
+]
+
+# Tipo Institución
+available_inst_type = [
+    'NO OFICIAL - CORPORACIÓN',
+    'NO OFICIAL - FUNDACIÓN',
+    'OFICIAL NACIONAL',
+    'OFICIAL DEPARTAMENTAL',
+    'OFICIAL MUNICIPAL',
+    'REGIMEN ESPECIAL'
+]
+
+# Caracter Institución
+available_inst_class = [
+    'UNIVERSIDAD',
+    'INSTITUCIÓN UNIVERSITARIA',
+    'INSTITUCIÓN TECNOLÓGICA',
+    'TÉCNICA PROFESIONAL',
+    'ESCUELA NORMAL SUPERIOR'
+]
+
+# Presentation Order
+presentation_order = {
+    'estu_horassemanatrabaja': available_work_week_hours,
+    'estu_genero': available_gender,
+    'fami_estratovivienda': available_estrato,
+    'estu_simulacrotipoicfes': available_yes_no,
+    'estu_metodo_prgm': available_method,
+    'inst_origen': available_inst_type,
+    'inst_caracter_academico': available_inst_class,
+    'estu_valormatriculauniversidad': available_tuition_cost
+    #'estu_nucleo_pregrado': [],
+}
+
+
+# -----------------
+# Tabs Styles
+
+tabs_styles = {
+    'height': '32px'
+}
+
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px 0px',
+    'fontWeight': 'bold'
+}
+
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#5DADEC',
+    'color': 'white',
+    'padding': '6px'
+}
+
 # -----------------
 # Layout
 app.layout = html.Div([
     
-    html.H1("ICFES Test Dashboard", style={'text-align':'center'}, id='Title'),
+    
+    # Title
+    html.H1("What factors drive Saber Pro and Saber TyT Test results?", style={'text-align':'center'}, id='Title'),
 
-    html.Div([
-        html.Label('Test'),
-        dcc.Dropdown(
-            id='filter-test',
-            options=[{'label': i.upper(), 'value': i} for i in available_test],
-            value=available_test[0]
+    # Subtitle
+    # html.H4("Saber Pro and Saber TyT", style={'text-align':'center'}, id='Subtitle'),
+
+    # Tabs
+    dcc.Tabs([
+        dcc.Tab(label=' Project Overview', children=[
+            
+        ],
+        style=tab_style,
+        selected_style=tab_selected_style
+        ),
+
+        dcc.Tab(label='Explore results', children=[
+            
+            # Row with filter and map
+            html.Div([
+                # Filter box
+                html.Div([
+
+                    html.Label('Test'),
+                    dcc.Dropdown(
+                        id='test-dropdown',
+                        options=[{'label': i.upper(), 'value': i} for i in available_test],
+                        value=available_test[0]
+                    ),
+
+                    html.Label('Module'),
+                    dcc.Dropdown(
+                        id='module-dropdown',
+                        options=[{'label': i.upper(), 'value': i} for i in available_module],
+                        value="mod_comuni_escrita_punt"
+                    ),
+
+                    html.Label('Factor'),
+                    dcc.Dropdown(
+                        id='factor-dropdown',
+                        options=[{'label': i.upper(), 'value': i} for i in available_factor],
+                        value="estu_genero"
+                    ),
+
+                    html.Label('Year'),
+                    dcc.Slider(
+                        id='year-slider',
+                        min=2016,
+                        max=2019,
+                        value=2016,
+                        marks={str(year): str(year) for year in available_year},
+                        step=None
+                    )
+
+                ],
+                className="pretty_container four columns",
+                id="cross-filter-options"
+                ),
+
+                # Graphs
+                html.Div([
+
+                    html.Div([
+                        dcc.Graph(id='graph-with-filter')
+                    ],
+                    className="pretty_container"),
+
+                ],
+                id="right-column",
+                className="eight columns"
+                )
+            
+            ],
+            className="row flex-display"
+            ),
+
+            # # Row for another graph
+            # html.Div([
+            #     #
+            #     html.Div([
+            #         dcc.Graph(id='another-graph')
+            #     ])
+            # ],
+            # className="pretty_container"),
+
+        ],
+        style=tab_style,
+        selected_style=tab_selected_style
+        ),
+
+        dcc.Tab(label='Predict your results', children=[],
+        style=tab_style,
+        selected_style=tab_selected_style
+        ),
+
+        dcc.Tab(label='About Team 18', children=[],
+        style=tab_style,
+        selected_style=tab_selected_style
         )
-    ]),
-
-
-    html.Div([
-        html.Label('Module'),
-        dcc.Dropdown(
-            id='yaxis-column',
-            options=[{'label': i.upper(), 'value': i} for i in available_module],
-            value="mod_comuni_escrita_punt"
-        )
-    ]),    
-
-    html.Div([
-        html.Label('Period'),
-        dcc.Slider(
-            id='year-slider',
-            min=2016,
-            max=2019,
-            value=2016,
-            marks={str(year): str(year) for year in available_year},
-            step=None
-        )
-    ]),
-
-    html.Div([
-        dcc.Graph(id='graph-with-slider'),
-        dcc.Graph(id='map')
-    ]),
-
-    html.Div(
-        id = "loading-chart"
-    )
+    ],
+    style=tabs_styles
+    ),
+    
 ])
 
 
 # -----------------
-# Callbacks
+# Graph Functions
 
-# Box Plot Callback
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    [Input('filter-test', 'value'),
-    Input('year-slider', 'value'),
-    Input('yaxis-column', 'value')])
-def update_figure(selected_test,selected_year, selected_mod):
+def get_box_plot(selected_test,selected_year, selected_mod, selected_factor):
     query = '''
-        SELECT fami_estratovivienda, {0} 
+        SELECT {3}, {0} 
         FROM datafinaldepurado 
         WHERE prueba = '{1}' 
         AND year = {2}
-    '''.format(selected_mod, selected_test, selected_year)
+    '''.format(selected_mod, selected_test, selected_year, selected_factor)
 
     filtered_df = pd.read_sql_query(query,con=engine)
     filtered_df.dropna(inplace=True)
 
-    var_partition = "fami_estratovivienda"
-    fig = px.box(filtered_df, x=var_partition, y=selected_mod, color=var_partition,
-                 category_orders={"fami_estratovivienda": available_estrato})
+    fig = px.box(filtered_df, x=selected_factor, y=selected_mod, color=selected_factor,
+                 category_orders=presentation_order)
     
-    fig.update_layout(transition_duration=2)
+    fig.update_layout(
+        transition_duration=2,
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=-0.4,
+            xanchor="center",
+            x=0.5,
+            title=""
+        )
+    )
+
+    fig.update_xaxes(showticklabels=False)
     fig.update_yaxes(title=selected_mod) 
 
     return fig
 
 
-# Map Callback
-@app.callback(
-    Output('map', 'figure'),
-    [Input('filter-test', 'value'),
-    Input('year-slider', 'value'),
-    Input('yaxis-column', 'value')])
-def update_map(selected_test,selected_year, selected_mod):
+def get_map(selected_test,selected_year, selected_mod):
     query = '''
         SELECT *
         FROM fact_estu_prgm_departamento
@@ -176,6 +346,32 @@ def update_map(selected_test,selected_year, selected_mod):
 
     fig.update_layout(title="Colombia",margin={"r":0,"t":0,"l":0,"b":0}, paper_bgcolor="#F8F9F9", plot_bgcolor="#F8F9F9")
     return fig
+
+
+# -----------------
+# Callbacks
+
+# Box Plot Callback
+@app.callback(
+    Output('graph-with-filter', 'figure'),
+    [Input('test-dropdown', 'value'),
+    Input('year-slider', 'value'),
+    Input('module-dropdown', 'value'),
+    Input('factor-dropdown', 'value')])
+def update_figure(selected_test,selected_year, selected_mod, selected_factor):
+    if selected_factor ==  'estu_prgm_departamento':
+        return get_map(selected_test,selected_year, selected_mod)
+    elif selected_factor in  ['fami_estratovivienda', 'estu_genero']:
+        return get_box_plot(selected_test,selected_year, selected_mod, selected_factor)
+    else:
+        return get_box_plot(selected_test,selected_year, selected_mod, selected_factor)
+    # if selected_factor ==  'estu_nucleo_pregrado':
+    # elif selected_factor ==  'inst_caracter_academico':
+    # elif selected_factor ==  'estu_metodo_prgm':
+    # elif selected_factor ==  'inst_origen':
+    # elif selected_factor ==  'estu_valormatriculauniversidad':
+    # elif selected_factor ==  'estu_simulacrotipoicfes':
+    # elif selected_factor ==  'estu_horassemanatrabaja':
 
 
 print('Loading app...')
