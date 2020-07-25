682c71699af4ad92d2e2aee77acb869d9caec2d3
Ubuntu <ubuntu@ip-172-31-5-225.us-west-2.compute.internal>
2020-07-25 20:53:45 +0000

    fixed bug in clean_estu_nucleo_pregrado.sql script

--- db/clean_estu_nucleo_pregrado.sql
+++ db/clean_estu_nucleo_pregrado.sql
@@ -1,8 +1,8 @@
 ALTER TABLE datafinaldepurado
-RENAME COLUMN estu_nucleo_pregrado to estu_nucleo_pregrado_old
+RENAME COLUMN estu_nucleo_pregrado to estu_nucleo_pregrado_old;
 
 ALTER TABLE datafinaldepurado
 ADD COLUMN estu_nucleo_pregrado TEXT;
-									
+
 UPDATE datafinaldepurado
-SET estu_nucleo_pregrado = trim(regexp_replace(estu_nucleo_pregrado_old, '[^[:alpha:]\s]', '', 'g'))
+SET estu_nucleo_pregrado = trim(regexp_replace(estu_nucleo_pregrado_old, '[^[:alpha:]\s]', '', 'g'));
